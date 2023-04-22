feceb3d6abe2f4268fc5290acfb846f26d2f56ec
Gr1shanya <128539548+Gr1shanya@users.noreply.github.com>
2023-04-22 13:02:17 +0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+vid=new Array(
+    '<iframe width="1120" height="630" src="https://www.youtube.com/embed/KCyLauoygPU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
+    '<iframe width="1120" height="630" src="https://www.youtube.com/embed/QAntPqwPx7s" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
+    '<iframe width="1120" height="630" src="https://www.youtube.com/embed/i7_SsqrShLA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>'
+    );
+    n=Math.floor(Math.random() * 3);
+    document.write(vid[n]);
